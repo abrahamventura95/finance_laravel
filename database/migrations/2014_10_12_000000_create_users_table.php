df5e0c9565010e93a471e1113322ac3385dd63d8
Abraham Ventura <amsventura.95@gmail.com>
2020-09-14 13:01:30 -0400

    Migrations
    Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email',125)->unique();
+            $table->enum('gender',['male','female','other']);
+            $table->enum('type',['personal','business']);
+            $table->integer('coin')->unsigned();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
